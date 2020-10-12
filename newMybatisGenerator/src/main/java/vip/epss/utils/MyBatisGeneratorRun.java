6df6d774b3f2e96795b20b035d57c849d0f6b942
wangzhanf <wangzhanf@126.com>
2020-10-12 21:57:41 +0800

    关于mybatis逆向工程的修改

--- newMybatisGenerator/src/main/java/vip/epss/utils/MyBatisGeneratorRun.java
+++ newMybatisGenerator/src/main/java/vip/epss/utils/MyBatisGeneratorRun.java
@@ -1,40 +1,26 @@
 package vip.epss.utils;
 
-
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MBGR {
-
-    public void generator() throws Exception {
+/**
+ * MyBatis的逆向工程执行文件
+ * */
+public class MyBatisGeneratorRun {
+    public static void main(String[] args) throws Exception {
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
-        File configFile = new File("D:\\work\\东软课程\\东软 社招_2020_JAVA_6班\\spring\\mybatisGenerator\\src\\main\\resources\\mybatisGR.xml");
+        File configFile = new File("D:\\work\\东软课程\\东软 社招_2020_JAVA_6班\\spring\\newMybatisGenerator\\src\\main\\resources\\mybatisGeneratorConf.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
-
     }
-
-    // 执行main方法以生成代码
-    public static void main(String[] args) throws IOException, XMLParserException {
-        try {
-            MBGR mbgr = new MBGR();
-            mbgr.generator();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
 }
